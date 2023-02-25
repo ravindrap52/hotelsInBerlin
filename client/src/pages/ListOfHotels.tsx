8942d724f9018c5b1c95f9c4def218046511fa4a
Ravindra <rp@shoutrlabs.com>
2023-02-25 23:34:58 +0100

    feat(frontend): allowing users to select the language

--- client/src/pages/ListOfHotels.tsx
+++ client/src/pages/ListOfHotels.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useAppSelector, useAppDispatch } from "../app/hooks";
 import {
   status,
@@ -7,7 +8,6 @@ import {
 } from "../features/hotels/hotelsSlice";
 
 import { ListItems } from "../components/molecules/ListItems";
-import { useEffect } from "react";
 
 //TODO improve styling
 export const ListOfHotels = () => {
@@ -16,11 +16,12 @@ export const ListOfHotels = () => {
   const hotels = useAppSelector(allHotels);
   const loadingStatus = useAppSelector(status);
   const errorMessage = useAppSelector(error);
-  const locale = useAppSelector(state => state.language.lang);
+  const locale = useAppSelector((state) => state.language.lang);
+  const [defaultLocale, setDefaultLocale] = useState(locale);
 
   useEffect(() => {
-    dispatch(getAllHotels(locale));
-  }, []);
+    dispatch(getAllHotels(defaultLocale));
+  }, [defaultLocale]);
 
   let content = null;
   if (loadingStatus === "loading") {
@@ -38,7 +39,21 @@ export const ListOfHotels = () => {
 
   return (
     <section>
-      <p>List of Hotels</p>
+      <label
+        htmlFor="languages"
+        className="block mb-2 text-sm font-medium text-gray-900"
+      >Select Language</label>
+      <select
+        id="languages"
+        value={defaultLocale}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 p-2.5"
+        onChange={(e) => setDefaultLocale(e.target.value)}
+      >
+        <option value="en-US">English</option>
+        <option value="de-DE">German</option>
+        <option value="fr-FR">France</option>
+        <option value="es-ES">Spanish</option>
+      </select>
       {content}
     </section>
   );
